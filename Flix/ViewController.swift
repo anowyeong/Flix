1a3aae229c239ead5ddf98165ae1e419c4891dcb
Anthony Owyeong <littledude96@gmail.com>
2019-02-07 16:42:54 -0800

    Adding completed part 1

---
+++ Flix/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Anthony Owyeong on 2/6/19.
+//  Copyright © 2019 Anthony Owyeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
