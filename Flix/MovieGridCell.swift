a3fcb79b842ece7120dcaa125cb89380b5ad75da
Anthony Owyeong <littledude96@gmail.com>
2019-02-15 17:50:17 -0800

    completed flix part 2 without bonus

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Anthony Owyeong on 2/15/19.
+//  Copyright © 2019 Anthony Owyeong. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+  
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
